5ebf4526b814254f222d910500baa37018c61715
IvanD <6766276+Ivan11114@users.noreply.github.com>
2024-10-26 19:10:15 +0200

    Add initial commit

---
+++ cups/install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade
+
+
+
+   1  sudo apt-get update
+    2  sudo apt-get upgrade
+    3  sudo usermod -a -G lpadmin ivan
+    4  sudo cupsctl --remote-any
+    5  sudo systemctl restart cups
+    6  ifconfig
+    7  sudo apt-get install samba
+    8  sudo nano /etc/samba/smb.conf
+    9  sudo systemctl restart samba
+   10  sudo systemctl restart smbd
+   11  sudo raspi-config
+   12  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   13  sudo wpa_cli -i wlan0 reconfigure
+   14  ifconfig
+   15  sudo wpa_cli -i wlan0 reconfigure
+   16  sudo raspi-config
+   17  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   18  wpa_passphrase "Vareny zemiak 3" | sudo tee -a /etc/wpa_supplicant/wpa_supplicant.conf
+   19  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   20  sudo wpa_cli -i wlan0 reconfigure
+   21  ifconfig
+   22  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   23  sudo wpa_cli -i wlan0 reconfigure
+   24  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   25  sudo wpa_cli -i wlan0 reconfigure
+   26  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   27  sudo wpa_cli -i wlan0 reconfigure
+   28  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   29  sudo wpa_cli -i wlan0 reconfigure
+   30  ifconfig
+   31  sudo raspi-config
+   32  ifconfig
+   33  sudo wpa_cli -i wlan0 reconfigure
+   34  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   35  sudo wpa_cli -i wlan0 reconfigure
+   36  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   37  sudo wpa_cli -i wlan0 reconfigure
+   38  sudo nano /etc/wpa_supplicant/wpa_supplicant.conf
+   39  sudo wpa_cli -i wlan0 reconfigure
+   40  sudo wpa_cli log_level debug
+   41  sudo wpa_cli -i wlan0 reconfigure
+   42  sudo wpa_cli -i wlan0 reconfigure log_level debug
+   43  sudo wpa_cli -i wlan0 reconfigure
+   44  sudo wpa_cli log_level debug
+   45  sudo wpa_cli log_level
+   46  sudo wpa_cli log_level error
+   47  sudo wpa_cli log_level
+   48  sudo wpa_cli -i wlan0 reconfigure
+   49  sudo raspi-config
+   50  nmtui
+   51  ifconfig
+   52  ping 10.1.1.1
+   53  poweroff
+   54  sudo raspi-config
+   55  nmtui
+   56  poweroff
+   57  sudo raspi-config
+   58  poweroff
+   59  sudo raspi-config
+   60  sudo nano /boot/config.txt
+   61  sudo nano /boot/firmware/config.txt
+   62  reboot
+   63  sudo reboot
+   64  ifconfig
+   65  ping 8.8.8.8
+   66  ifconfig
+   67  exit
+   68  hitory
+   69  history
+   70  history > save_hist.txt
+
